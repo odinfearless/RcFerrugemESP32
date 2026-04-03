65ee06a82027a1340b736e8f1e775fa787d7a66d
odinfearless <emygdio.alex@gmail.com>
2026-04-03 01:56:12 -0300

    adicionado starter

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,8 @@ const unsigned int MOTOR_PWM_1 = 8;
 const unsigned int MOTOR_PWM_2 = 9;
 
 // ================== CONFIG SENSORES ==================
-const unsigned int SENSOR_TrigPin_FL = 4;
-const unsigned int SENSOR_EchoPin_FL = 5;
+const unsigned int SENSOR_TrigPin_FL = 35;
+const unsigned int SENSOR_EchoPin_FL = 36;
 
 const unsigned int SENSOR_TrigPin_FR = 6;
 const unsigned int SENSOR_EchoPin_FR = 7;
@@ -27,27 +27,29 @@ const unsigned int SENSOR_EchoPin_BL = 16;
 const unsigned int SENSOR_TrigPin_BR = 17;
 const unsigned int SENSOR_EchoPin_BR = 18;
 
+const unsigned int SENSOR_TrigPin_SL = 35;
+const unsigned int SENSOR_EchoPin_SL = 36;
+
+const unsigned int SENSOR_TrigPin_SR = 37;
+const unsigned int SENSOR_EchoPin_SR = 38;
+
 const unsigned int deadZonePwm = 120;
-const unsigned int maxLimitPwm = 255;
+const unsigned int maxLimitPwm = 220;
 
-// ================== CONFIG POTENCIÔMETROS ==================
-const unsigned long TimeReleasePots = 1000;
+// ================== CONFIG POTs  ==================
+const unsigned int PotDistancePin = 3;
+const unsigned int PotPwmPin = 5;
+
+// ================== CONFIG  ==================
+const unsigned int releaseTimePots = 1000;
 const unsigned int AnalogMaxValue = 4095;
+const unsigned int minLimitDistance = 3;
+const unsigned int maxLimitDistance = 40;
 
-// 1 - POT
-int distanceDetection = 50;
-const unsigned int PotDistancePin = 1; 
-const unsigned int minLimitDistance = 5;
-const unsigned int maxLimitDistance = 80;
-unsigned long potLeftlastMillis = 0;
-
-// 2 - POT
-int currentMaxLimitPwm = 0;
-const unsigned int PotPwmPin = 2; 
-unsigned long potRightlastMillis = 0;
+//==================== Starter ==================
+const unsigned int Starter_Pin = 1;
 
 // ==================== Motores ========================
-
 Motor Motor_A(MOTOR_1_A, MOTOR_1_B, MOTOR_PWM_1, maxLimitPwm, deadZonePwm, 0);
 Motor Motor_B(MOTOR_2_A, MOTOR_2_B, MOTOR_PWM_2, maxLimitPwm, deadZonePwm, 1);
 
@@ -58,13 +60,21 @@ SonarSensor Front_Sensor_FR(SENSOR_TrigPin_FR, SENSOR_EchoPin_FR);
 SonarSensor Front_Sensor_BL(SENSOR_TrigPin_BL, SENSOR_EchoPin_BL);
 SonarSensor Front_Sensor_BR(SENSOR_TrigPin_BR, SENSOR_EchoPin_BR);
 
-// ====================== Variaveis Local =====================
+SonarSensor Side_Sensor_L(SENSOR_TrigPin_SL, SENSOR_EchoPin_SL);
+SonarSensor Side_Sensor_R(SENSOR_TrigPin_SR, SENSOR_EchoPin_SR);
 
+// ====================== Variáveis =====================
+volatile bool isTurn = false;
 volatile bool isBackward = false;
-volatile bool isForward = false;
-volatile int currentPwm = 0;
+volatile float currentPwm = 0;
+volatile unsigned long potsLastMillis = 0;
+volatile int distanceDetection = maxLimitDistance;
+volatile int currentMaxLimitPwm = maxLimitPwm;
+volatile bool tasksRunning = false;
+volatile bool lastState = HIGH;
+static unsigned long lastTrigger = 0;
 
-TaskHandle_t taskSensors;
+TaskHandle_t taskForward;
 TaskHandle_t taskBackward;
 TaskHandle_t taskCommands;
 TaskHandle_t taskPots;
@@ -80,56 +90,97 @@ enum Commands
 
 volatile Commands currentCommand = STOP;
 
-void taskSensorsLoop(void *pvParameters)
+float lerp(float a, float b, float t)
+{
+  return a + t * (b - a);
+}
+
+void taskStarterSensorLoop(void *pvParameters)
 {
   while (true)
   {
+    bool currentState = digitalRead(Starter_Pin);
+
+    if (lastState == HIGH && currentState == LOW &&
+        millis() - lastTrigger > 300)
+    {
+      lastTrigger = millis();
+
+      tasksRunning = !tasksRunning;
+
+      if (tasksRunning)
+      {
+        Serial.println("Tasks START");
+      }
+      else
+      {
+
+        Serial.println("Tasks STOP");
+      }
+    }
+
+    lastState = currentState;
+    vTaskDelay(pdMS_TO_TICKS(30));
+  }
+}
+// ===================== SENSORES =======================
+void taskForwardLoop(void *pvParameters)
+{
+  while (true)
+  {
+    if (!tasksRunning)
+    {
+      Motor_A.setValue(0);
+      Motor_B.setValue(0);
+      vTaskDelay(pdMS_TO_TICKS(50));
+      continue;
+    }
+
     int distanceFL = Front_Sensor_FL.read();
     int distanceFR = Front_Sensor_FR.read();
 
     int motorDistanceSensor = min(distanceFL, distanceFR);
 
-    float speedNorm = (float)currentMaxLimitPwm / maxLimitPwm;
-    int dynamicMinDistance = 10 + (speedNorm * speedNorm) * 30;
-
-    int dynamicDistance = map(currentMaxLimitPwm, deadZonePwm, maxLimitPwm, minLimitDistance, distanceDetection);
-
-    float distNorm = (float)motorDistanceSensor / dynamicDistance;
-    distNorm = constrain(distNorm, 0.0, 1.0);
-
-    float curva = distNorm * distNorm * distNorm;
-
-    int minPwm = deadZonePwm;
-    int vlPwm = minPwm + curva * (currentMaxLimitPwm - minPwm);
-
-    // decisão global (variáveis compartilhadas)
-    if (motorDistanceSensor < dynamicMinDistance)
+    if (motorDistanceSensor < distanceDetection)
     {
       if (motorDistanceSensor < minLimitDistance)
       {
         isBackward = true;
       }
 
-      if (distanceFR < distanceFL)
-        currentCommand = LEFT;
-      else
-        currentCommand = RIGHT;
+      if (!isTurn)
+      {
+        isTurn = true;
+        if (distanceFR < distanceFL)
+          currentCommand = LEFT;
+        else
+          currentCommand = RIGHT;
+      }
     }
     else
     {
+      isTurn = false;
       isBackward = false;
-      currentPwm = vlPwm;
       currentCommand = FORWARD;
     }
 
-    vTaskDelay(20 / portTICK_PERIOD_MS); // 50Hz
+    vTaskDelay(pdMS_TO_TICKS(20));
   }
 }
 
+// ===================== BACKWARD =======================
 void taskBackwardLoop(void *pvParameters)
 {
   while (true)
   {
+    if (!tasksRunning)
+    {
+      Motor_A.setValue(0);
+      Motor_B.setValue(0);
+      vTaskDelay(pdMS_TO_TICKS(50));
+      continue;
+    }
+
     if (isBackward)
     {
       int distanceBL = Front_Sensor_BL.read();
@@ -137,55 +188,76 @@ void taskBackwardLoop(void *pvParameters)
 
       int motorDistanceSensor = min(distanceBL, distanceBR);
 
-      currentCommand = BACKWARD;
-
-      if (motorDistanceSensor < 20)
+      if (motorDistanceSensor <= minLimitDistance)
       {
-        if (distanceBR < distanceBL)
-          currentCommand = RIGHT;
-        else
-          currentCommand = LEFT;
+        currentCommand = FORWARD;
+      }
+      else
+      {
+        currentCommand = BACKWARD;
       }
     }
-    vTaskDelay(30 / portTICK_PERIOD_MS);
+
+    vTaskDelay(pdMS_TO_TICKS(20));
   }
 }
 
+// ===================== POTENCIÔMETROS =======================
 void taskPotsLoop(void *pvParameters)
 {
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  const TickType_t xFrequency = pdMS_TO_TICKS(1000); // 100ms
+
   while (true)
   {
+    if (!tasksRunning)
+    {
+      vTaskDelay(pdMS_TO_TICKS(50));
+      continue;
+    }
+
     int potLeft = analogRead(PotDistancePin);
-    distanceDetection = map(potLeft, 0, AnalogMaxValue, minLimitDistance, maxLimitDistance);
+    distanceDetection = map(potLeft, 0, AnalogMaxValue,
+                            minLimitDistance, maxLimitDistance);
 
     int potRight = analogRead(PotPwmPin);
-    currentMaxLimitPwm = map(potRight, 0, AnalogMaxValue, deadZonePwm, maxLimitPwm);
+    currentMaxLimitPwm = map(potRight, 0, AnalogMaxValue,
+                             deadZonePwm, maxLimitPwm);
 
-    vTaskDelay(200 / portTICK_PERIOD_MS);
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
   }
 }
 
+// ===================== COMANDOS =======================
 void taskCommandsLoop(void *pvParameters)
 {
   while (true)
   {
+    if (!tasksRunning)
+    {
+      Motor_A.setValue(0);
+      Motor_B.setValue(0);
+      vTaskDelay(pdMS_TO_TICKS(50));
+      continue;
+    }
+
     switch (currentCommand)
     {
     case LEFT:
-      Motor_A.setValue(deadZonePwm);
-      Motor_B.setValue(0);
+      Motor_A.setValue(0);
+      Motor_B.setValue(deadZonePwm);
       break;
 
     case RIGHT:
-      Motor_A.setValue(0);
-      Motor_B.setValue(deadZonePwm);
+      Motor_A.setValue(deadZonePwm);
+      Motor_B.setValue(0);
       break;
 
     case FORWARD:
       Motor_A.forward();
       Motor_B.forward();
-      Motor_A.setValue(currentPwm);
-      Motor_B.setValue(currentPwm);
+      Motor_A.setValue(currentMaxLimitPwm);
+      Motor_B.setValue(currentMaxLimitPwm);
       break;
 
     case BACKWARD:
@@ -194,16 +266,18 @@ void taskCommandsLoop(void *pvParameters)
       Motor_A.setValue(deadZonePwm);
       Motor_B.setValue(deadZonePwm);
       break;
-    default:
+
+    case STOP:
       Motor_A.setValue(0);
       Motor_B.setValue(0);
       break;
     }
 
-    vTaskDelay(20 / portTICK_PERIOD_MS);
+    vTaskDelay(pdMS_TO_TICKS(10));
   }
 }
 
+// ===================== SETUP =======================
 void setup()
 {
   Serial.begin(115200);
@@ -212,25 +286,26 @@ void setup()
   WiFi.mode(WIFI_OFF);
   btStop();
 
+  /*
+  pinMode(Starter_Pin, INPUT_PULLUP);
+
   Front_Sensor_FL.begin();
   Front_Sensor_FR.begin();
   Front_Sensor_BL.begin();
   Front_Sensor_BR.begin();
-
   Motor_A.begin();
   Motor_B.begin();
 
-  pinMode(PotDistancePin, INPUT);
-  pinMode(PotPwmPin, INPUT);
-
-  xTaskCreatePinnedToCore(taskSensorsLoop, "Sensors", 4096, NULL, 2, &taskSensors, 0);
-
+  xTaskCreatePinnedToCore(taskStarterSensorLoop, "StarterSensor", 2048, NULL, 3, NULL, 1);
+  xTaskCreatePinnedToCore(taskForwardLoop, "Forward", 4096, NULL, 2, &taskForward, 0);
   xTaskCreatePinnedToCore(taskBackwardLoop, "Backward", 4096, NULL, 1, &taskBackward, 1);
-
-  xTaskCreatePinnedToCore(taskCommandsLoop, "Commands", 4096, NULL, 1, &taskCommands, 1);
-
+  xTaskCreatePinnedToCore(taskCommandsLoop, "Commands", 2048, NULL, 1, &taskCommands, 1);
   xTaskCreatePinnedToCore(taskPotsLoop, "Pots", 2048, NULL, 1, &taskPots, 1);
+  */
 }
+
 void loop()
 {
+
+  
 }
